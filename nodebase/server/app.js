b025ff29500997c4d40c1cb498b15985b0bac487
yuri <15659149471@163.com>
2024-01-09 21:08:51 +0800

    编码迭代

--- nodebase/server/app.js
+++ nodebase/server/app.js
@@ -1,13 +1,35 @@
 const express = require('express')
 const cors = require('cors')
 const userRouter = require('./router/user')
+const Joi = require('joi')
 
 const app = express()
 app.use(cors()) // 跨域问题
 app.use(express.urlencoded({extended: false })) // 只能处理 application/x-www-form-urlencode
-
+app.use((req, res, next) => {
+  res.cc = (status, msg) => { // 注册集中处理函数
+    res.send({
+      status,
+      msg
+    })
+  }
+  next()
+})
 app.listen(8081, () => {
   console.log('server listen at 8081')
 })
 
 app.use('/api', userRouter) // 挂载前缀
+
+app.use((err, req, res, next) => {
+  if (err instanceof Joi.ValidationError) {
+    return res.send({
+      status: 500000,
+      message: err.message
+    })
+  }
+  res.send({
+    status: 500000,
+    message: err.message
+  })
+})
--- nodebase/server/router/user.js
+++ nodebase/server/router/user.js
@@ -2,8 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 const userHandler = require('../router_handler/user')
+const Joi = require('joi') // 定义验证规则
+const expressJoi = require('@escook/express-joi') // 实践规则
 
-router.post('/reguser', userHandler.reguser)
+const userScheme = {
+  body: {
+    username: Joi.string().alphanum().min(1).max(12).required(), // a-z 0-9  长度范围 3 - 12
+    password: Joi.string().pattern(/^[\S]{6, 15}$/).required() // 非空格 6 到15位字符
+  }
+}
+
+router.post('/reguser', expressJoi(userScheme), userHandler.reguser)
 
 router.post('/login', userHandler.login)
 
