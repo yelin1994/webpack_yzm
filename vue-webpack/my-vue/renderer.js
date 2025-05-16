a1d6d1a1a9b5992e7449cda8b345a962fb5396a1
yuri <15659149471@163.com>
2025-05-16 18:14:33 +0800

    编译器 编码

---
+++ vue-webpack/my-vue/renderer.js
@@ -0,0 +1,139 @@
+function createRenderer(options) {
+  const {
+    createElement,
+    setElementText,
+    insert,
+    patchProps
+  } = options
+  function render(vnode, container) {
+    if (vnode) {
+      patch(container._vnode, vnode, container)
+    } else {
+      // 说明是卸载操作
+      if (container._vnode) {
+        //  container.innnerHtml = ''
+        // let el = container._vnode.el
+        // let parent = el.parentNode
+        // if (parent) parent.removeChild(el)
+        unmount(container._vnode)
+      }
+     
+    }
+    container._vnode = vnode
+  }
+  function hydrate(vnode, container) {
+
+  }
+
+
+  function patch(oldnode, vnode, container) {
+    if (oldnode && oldnode.type !== vnode.type) {
+      //如果新旧node 的类型不一致 则将 旧node 卸载
+      unmount(oldnode)
+      oldnode = null
+    }
+
+    const { type } = vnode
+    if (typeof type === 'string') { // 普通标签
+      if (!oldnode) {
+        mountElement(vnode, container)
+      } else {
+        patchElement(oldnode, vnode)
+      }
+    } else if (typeof type === 'object') { //自定义组件
+
+    } else if (type === 'xxx') {
+
+    }
+   
+  }
+
+  function mountElement(vnode, container) {
+    // vnode.el 指向 真实DOM元素
+    const el = vnode.el =  createElement(vnode.type)
+    if (vnode.props) {
+      for (let key in vnode.props) {
+         const value = vnode.props[key]
+        // 用 in 来判断 是否存在对应的DOM Properties
+        patchProps(el, key, null, value)
+      }
+    }
+    if (typeof vnode.children === 'string') { // 如果子节点是文本节点
+      // el.textContent = vnode.children
+      setElementText(el, vnode.children)
+    } else if (Array.isArray(vnode.children)) {
+      vnode.children.forEach(child => {
+        patch(null, child, el)
+      })
+    }
+    insert(vnode, container)
+  }
+
+  function unmount(vnode) {
+    let parent = vnode.el.parentNode
+    if (parent) parent.removeChild()
+  }
+
+  function patchElement(oldnode, vnode) {
+
+  }
+
+  return {
+    render,
+    hydrate
+  }
+}
+
+const renderer = createRenderer({
+  createElement(tag) {
+    return document.createElement(tag)
+  },
+  setElementText(el, text) {
+    el.textContent = text
+  },
+  insert(el, parent, anchor = null) {
+    parent.insertBefore(el, anchor)
+  },
+  patchProps(el, key, preValue, nextValue) {
+    if (/^on/.test(key)) { // 事件处理
+      let name = key.slice(2).toLowerCase()
+      // preValue && el.removeListener(name, preValue)
+      // el.addEventListener(name, nextValue) 
+
+      let invokers = el._vei || (el._vei = {}) // 获取该元素伪造的事件处理函数 vei 是 vue event invoker
+      if (nextValue) {
+        if (!invoker) {
+          invoker = el._vei = (e) => {
+            // 当伪造的处理函数执行时 会执行真正的函数
+            invoker.value(e)
+          }
+          invoker.value = nextValue // 绑定真正的函数
+          el.addEventListener(name, invoker)
+        } else {
+          invoker.value = nextValue // 不用移除旧函数
+        }
+      } else {
+        el.removeListener(name, invoker)
+      }
+    } else if (key === 'class') {
+      el.className = nextValue // 也可以用el.classList 设置
+    } else if (shouldSetAsProps(el, key, value)) {
+      // 获取 DOM 对应  property 的类型
+      const type = typeof el[key]
+      if (type === 'boolean' && value === '') {
+        el[key] = true
+      } else {
+        el[key] = value
+      }
+    } else {
+      el.setAttribute(key, vnode.props[key])
+    }
+  }
+})
+
+
+  function shouldSetAsProps(el, key, value) {
+    // 特殊处理 只读属性 无法通过 DOM property = xx 设置
+    if (key === 'form' && el.tagName === 'INPUT') return false
+    return key in el
+  }
--- vue-webpack/my-vue/watchRef.js
+++ vue-webpack/my-vue/watchRef.js
@@ -10,3 +10,50 @@ function ref(val) {
   })
   return reactive(wrapper)
 }
+
+// 主要是问了解决响应式丢失问题
+
+function toRef(obj, key) {
+  const wrappger = {
+    get value() {
+      return obj[key]
+    },
+    set value(val) {
+      obj[key] = val
+    }
+  }
+  Object.defineProperty(wrapper, '__v_isRef', {
+    value: true
+  })
+  return wrapper
+}
+
+function toRefs(obj) {
+  const ret = {}
+  for (let key of obj) {
+    ret[key] = ref(obj, key)
+  }
+  return ret
+}
+// setup() 会默认调用proxyRefs
+function proxyRefs(target) {
+  return new Proxy(target, {
+    get(target, key, receiver) {
+      const value = Reflect.get(target, key, receiver)
+      if (value.__v_isRef) return value.value
+      return value
+    },
+    set(target, key, newvalue, receiver) {
+      const value = target[key]
+      if (value.__v_isRef) {
+        value.value = newvalue
+        return true
+      }
+      return Reflect.set(target, key, newvalue, receiver)
+    }
+  })
+}
+
+
+const obj = reactive({ foo: 1, bar: 2 })
+const newObj = proxyRefs({...toRefs(obj)})
